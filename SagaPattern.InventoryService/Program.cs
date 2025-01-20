a5dbce99dd7ee2d8bfb427b3ee41eb815f4ae16f
Florian Lorca <florca-ext@citegestion.fr>
2025-01-20 16:39:47 +0100

    Ajoutez des fichiers projet.

---
+++ SagaPattern.InventoryService/Program.cs
@@ -0,0 +1,49 @@
+using MassTransit;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddMassTransit(x =>
+{
+    // Pour le PaymentService
+
+    // Pour l'InventoryService
+    x.AddConsumer<PaymentProcessedEventConsumer>();
+
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("localhost", "/", h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+
+        // Configuration des endpoints
+        cfg.ConfigureEndpoints(context);
+    });
+});
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ SagaPattern.InventoryService/Worker.cs
@@ -0,0 +1,25 @@
+﻿using MassTransit;
+namespace SagaPattern.InventoryService
+{
+
+
+    public class Worker : BackgroundService
+    {
+        readonly IBus _bus;
+
+        public Worker(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await _bus.Publish(new { Value = $"The time is {DateTimeOffset.Now}" }, stoppingToken);
+
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
